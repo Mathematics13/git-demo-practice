063ad0c16b68dbbe034ffa328e4c242f64f3c38d
Mathematics13 <haripriyakrishnan4@gmail.com>
2024-01-27 15:14:09 +0000

    Commit2

--- src/Sana.java
+++ src/Sana.java
@@ -1,5 +1,7 @@
-public class Sana {
-    public static void main(String[] args) {
+public class Sana
+{
+    public static void main(String[] args)
+    {
         System.out.println("Sana git test");
     }
 }
